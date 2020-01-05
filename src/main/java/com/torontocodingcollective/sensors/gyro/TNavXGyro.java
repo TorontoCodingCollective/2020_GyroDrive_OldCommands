86dfb9872b925fc85c0eb96ddbd5263e370dc07e
Richard McMullin <richard.e.mcmullin@gmail.com>
2020-01-05 18:33:03 -0500

    Initial commit

---
+++ src/main/java/com/torontocodingcollective/sensors/gyro/TNavXGyro.java
@@ -0,0 +1,52 @@
+package com.torontocodingcollective.sensors.gyro;
+
+import com.kauailabs.navx.frc.AHRS;
+
+import edu.wpi.first.wpilibj.SPI.Port;
+
+public class TNavXGyro extends TGyro {
+
+    private final AHRS navXGyro;
+
+    public TNavXGyro() {
+        this(false);
+    }
+
+    public TNavXGyro(boolean inverted) {
+        super(inverted);
+        this.navXGyro = new AHRS(Port.kMXP);
+    }
+
+    @Override
+    public void calibrate() {
+        super.setGyroAngle(0);
+    }
+
+    @Override
+    public void close() throws Exception {
+        if (this.navXGyro != null) {
+            this.navXGyro.close();
+        }
+    }
+
+    @Override
+    public double getAngle() {
+        return super.getAngle(navXGyro.getAngle());
+    }
+
+    @Override
+    public double getPitch() {
+        return navXGyro.getRoll();
+    }
+
+    @Override
+    public double getRate() {
+        return super.getRate(navXGyro.getRate());
+    }
+
+    @Override
+    public boolean supportsPitch() {
+        return true;
+    }
+
+}
